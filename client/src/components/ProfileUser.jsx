f5099d26ff668902f74cf2ea1df5eee6194e533d
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-06-15 13:34:16 -0500

    created card for user info

--- client/src/components/ProfileUser.jsx
+++ client/src/components/ProfileUser.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
+import Paper from '@material-ui/core/Paper';
 import femaleAvatar from "../assets/images/femaleavatar.png"
 import "../App.css"
 
@@ -8,24 +9,38 @@ const useStyles = makeStyles((theme) => ({
 	root: {
 	  display: 'flex',
 	  '& > *': {
-		margin: theme.spacing(1),
+		margin: "auto",
 	  },
 	},
 	large: {
 	  width: theme.spacing(7),
 	  height: theme.spacing(7),
 	},
+	rootCard: {
+		display: 'flex',
+		flexWrap: 'wrap',
+		'& > *': {
+		  margin: theme.spacing(1),
+		  width: theme.spacing(32),
+		  height: theme.spacing(32),
+		  paddingTop: "5%"
+		},
+	  },
   }));
 
 export default function ProfileUser() {
 	const classes = useStyles();
 	return 	<div>
-			<div className="info-card">
+		<div className="info-card">
+			<div className={classes.rootCard}>
+			<Paper elevation={3} >
 				<div className={classes.root}>
       <Avatar alt="Remy Sharp" src={femaleAvatar} className={classes.large} />
     			</div>
 	  <h3>Whitney Svoboda</h3>
 	  <p>Grapevine, TX</p>
+			</Paper>
+			</div>
 			</div>
 			</div>;
 }
