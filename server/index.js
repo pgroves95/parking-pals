63da4f4f24b4989bb07007d9f7e8d7dc54d2e6ab
Jessica Haugen <j.rose.haugen@gmail.com>
2021-06-15 08:51:02 -0700

    db
    dbInfo

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require("path");
 app.use(express.json());
 const PORT = process.env.PORT || 3001;
+const DBInfo = require("./DBcode");
+const CONNECTION_STRING = DBInfo;
 
 app.get("/api", (req, res) => {
 	res.json({ message: "AMAZINGGGGGG!!!" });
