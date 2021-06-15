7891277b0fe10caff1c61b2831fd765670e77933
Patrick Groves <patrickalexgroves@gmail.com>
2021-06-15 13:06:22 -0600

    query test in node

--- server/index.js
+++ server/index.js
@@ -3,8 +3,28 @@ const app = express();
 const path = require("path");
 app.use(express.json());
 const PORT = process.env.PORT || 3001;
-const DBInfo = require("./DBcode");
-const CONNECTION_STRING = DBInfo;
+const pg = require("pg");
+const CONNECTION_STRING = require("./DBcode");
+
+const client = new pg.Client(CONNECTION_STRING);
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].theTime);
+  });
+  client.query('SELECT * FROM "Users"', function(err,result) {
+	if(err) {
+		return console.error('error running query', err);
+	  }
+	  console.log(result);
+	  client.end();
+  })
+});
 
 app.get("/api", (req, res) => {
 	res.json({ message: "AMAZINGGGGGG!!!" });
