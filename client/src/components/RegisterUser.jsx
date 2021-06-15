761b66bde91dc85d8a7847dd35b825e3a7377bfc
Calvin Kling <klingcal@gmail.com>
2021-06-15 14:08:21 -0400

    user reg

--- client/src/components/RegisterUser.jsx
+++ client/src/components/RegisterUser.jsx
@@ -1,5 +1,46 @@
 import React from "react";
+import {
+	HostForm,
+	SignUpButton,
+	SignUpFormInput,
+	RegisterDiv,
+} from "../styledComponents/SignUpFormStyles";
+import "../App.css";
 
 export default function RegisterUser() {
-	return <div></div>;
+	return (
+		<RegisterDiv>
+			<div className="host-wrapper">
+				<h1>Sign Up to Start Parking</h1>
+				<HostForm>
+					<SignUpFormInput
+						id="first-name"
+						type="text"
+						placeholder="First Name"
+					/>
+					<SignUpFormInput id="last-name" type="text" placeholder="Last Name" />
+					<SignUpFormInput
+						id="email"
+						type="email"
+						placeholder="Email Address"
+					/>
+					<SignUpFormInput
+						id="street"
+						type="text"
+						placeholder="Street Address"
+					/>
+					<SignUpFormInput
+						id="apt-suite"
+						type="text"
+						placeholder="Apt/Suite #"
+					/>
+					<SignUpFormInput id="city" type="text" placeholder="City" />
+					<SignUpFormInput id="state" type="text" placeholder="State" />
+					<SignUpFormInput id="zip" type="text" placeholder="Zip Code" />
+					<SignUpFormInput type="text" placeholder="License Plate Number" />
+					<SignUpButton id="submit-button">Submit Form</SignUpButton>
+				</HostForm>
+			</div>
+		</RegisterDiv>
+	);
 }
