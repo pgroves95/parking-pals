3e2b9b05d6ccb9695ff1bc6a17b5ac4864a999ba
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-06-16 10:08:51 -0500

    make changes to sign up forms

--- client/src/components/RegisterHost.jsx
+++ client/src/components/RegisterHost.jsx
@@ -8,54 +8,51 @@ export default function RegisterHost() {
 			<div className="host-wrapper">
 				<h1>Register to Host Now</h1>
 
-				<div id="register-host-form" action="/driveway" method="POST">
+				<form id="register-host-form" action="/register" method="POST">
 					<input
+						name="firstName"
 						class="register-input"
 						id="first-name"
 						type="text"
 						placeholder="First Name"
 					/>
 					<input
+						name="lastName"
 						class="register-input"
 						id="last-name"
 						type="text"
 						placeholder="Last Name"
 					/>
 					<input
+						name="email"
 						class="register-input"
 						id="email"
 						type="email"
 						placeholder="Email Address"
 					/>
-
 					<input
 						class="register-input"
-						name="total_spaces"
-						id="spots"
-						type="number"
-						placeholder="Number of Parking Spots"
-					/>
-					<input
-						class="register-input"
-						name="address"
+						name="street"
 						id="street"
 						type="text"
 						placeholder="Street Address"
 					/>
 					<input
 						class="register-input"
-    name="city"
+    					name="city"
 						id="city"
 						type="text"
 						placeholder="City"
 					/>
 					<input
+						name="state"
 						class="register-input"
 						id="state"
 						type="text"
 						placeholder="State"
 					/>
 					<input
+						name="zip"
 						class="register-input"
 						id="zip"
 						type="text"
@@ -63,22 +60,28 @@ export default function RegisterHost() {
 					/>
 					<input
 						class="register-input"
-    name="rate"
+    					name="rate"
 						type="number"
 						placeholder="Hourly Parking Rate"
 					/>
 					<input
+						class="register-input"
+    					name="title"
+						type="text"
+						placeholder="Listing Title"
+					/>
+					<input
 						class="register-input"			
 						id="description"
-    name="description"
+    					name="description"
 						type="textarea"
 						placeholder="Driveway Description"
 					/>
-<input name="access" value="host" type="hidden"/>
+					<input name="access" value="host" type="hidden"/>
 					<button class="register-button" type="submit" id="submit-button">
 						Submit Form
 					</button>
-				</div>
+				</form>
 			</div>
 		</div>
 	);
--- client/src/components/RegisterUser.jsx
+++ client/src/components/RegisterUser.jsx
@@ -1,66 +1,57 @@
 import React from "react";
-import {
-	HostForm,
-	SignUpButton,
-	SignUpFormInput,
-	RegisterDiv,
-} from "../styledComponents/SignUpFormStyles";
 import "../App.css";
 
 export default function RegisterUser() {
 	return (
-		<RegisterDiv>
+		<div id="register-section">
 			<div className="host-wrapper">
 				<h1>Sign Up to Start Parking</h1>
-				<HostForm action="/register" method="POST">
-					<SignUpFormInput
-						name="name"
+				<form action="/register" method="POST">
+					<input
+						name="firstName"
 						id="first-name"
 						type="text"
 						placeholder="First Name"
 					/>
-					<SignUpFormInput id="last-name" type="text" placeholder="Last Name" />
-					<SignUpFormInput
+					<input name="lastName" id="last-name" type="text" placeholder="Last Name" />
+					<input
 						name="email"
 						id="email"
 						type="email"
 						placeholder="Email Address"
 					/>
-					<SignUpFormInput
-						id="email2"
-						type="email"
-						placeholder="Confirm Email Address"
-					/>
-					<SignUpFormInput
-					name="password"
+					<input
+						name="password"
 						id="password"
 						type="password"
 						placeholder="Password"
 					/>
-					<SignUpFormInput
+					<input
 						id="password2"
 						type="password"
 						placeholder="Confirm Password"
 					/>
-					<SignUpFormInput
+					<input
+						name="street"
 						id="street"
 						type="text"
 						placeholder="Street Address"
 					/>
-					<SignUpFormInput
+					<input
+						name="apt"
 						id="apt-suite"
 						type="text"
 						placeholder="Apt/Suite #"
 					/>
-					<SignUpFormInput id="city" type="text" placeholder="City" />
-					<SignUpFormInput id="state" type="text" placeholder="State" />
-					<SignUpFormInput id="zip" type="text" placeholder="Zip Code" />
-					<SignUpFormInput name="license_plate" type="text" placeholder="License Plate Number" />
-					<SignUpFormInput name="phone" id="phone" type="text" placeholder="Phone Number" />
+					<input name="city" id="city" type="text" placeholder="City" />
+					<input name="state" id="state" type="text" placeholder="State" />
+					<input name="zip" id="zip" type="text" placeholder="Zip Code" />
+					<input name="license_plate" type="text" placeholder="License Plate Number" />
+					<input name="phone" id="phone" type="text" placeholder="Phone Number" />
 					<input name="access" value="user" type="hidden"/>
-					<SignUpButton id="submit-button">Submit Form</SignUpButton>
-				</HostForm>
+					<button id="submit-button">Submit Form</button>
+				</form>
 			</div>
-		</RegisterDiv>
+		</div>
 	);
 }
