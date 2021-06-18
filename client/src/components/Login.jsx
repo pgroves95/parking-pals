75c4a146d412edbcc55fdc3c8c8567609a6241a1
Calvin Kling <klingcal@gmail.com>
2021-06-18 16:42:22 -0400

    updates

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 import "../css/Login.css";
 import "../css/RegisterHost.css";
 import dog from "../assets/images/dog.png";
+import { getLoginStatus } from "../actions/login-actions.js";
 
 export default function Login() {
 	const [email, setEmail] = React.useState("");
 	const [password, setPassword] = React.useState("");
+	// const [error, setError] = useState("");
 
 	const sendForm = (e) => {
 		e.preventDefault();
@@ -19,39 +21,38 @@ export default function Login() {
 			},
 		})
 			.then((res) => res.json())
-			.then(console.log);
+			.then((data) => console.log(data));
 
 		return false;
 	};
-  return (
-    <div>
-      <div id="login-section">
-        <form className="login-form" onSubmit={(e) => sendForm(e)}>
-          <h1 className="login-header">Log In to Find Parking</h1>
-          <img id="dog-icon" src={dog} alt="dog" />
-          <input
-            className="login-input-1"
-            type="email"
-            placeholder="Email"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-          />
-          <input
-            className="login-input-2"
-            type="password"
-            placeholder="Password"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-          />
-          <button className="login-button" type="submit">
-            Log In
-          </button>
-          <Link className="register-link" to="/RegisterUser">
-            <p>Not signed up? Click here to make an account!</p>
-          </Link>
-        </form>
-      </div>
-    </div>
-  );
-
+	return (
+		<div>
+			<div id="login-section">
+				<form className="login-form" onSubmit={(e) => sendForm(e)}>
+					<h1 className="login-header">Log In to Find Parking</h1>
+					<img id="dog-icon" src={dog} alt="dog" />
+					<input
+						className="login-input-1"
+						type="email"
+						placeholder="Email"
+						onChange={(e) => setEmail(e.target.value)}
+						value={email}
+					/>
+					<input
+						className="login-input-2"
+						type="password"
+						placeholder="Password"
+						onChange={(e) => setPassword(e.target.value)}
+						value={password}
+					/>
+					<button className="login-button" type="submit">
+						Log In
+					</button>
+					<Link className="register-link" to="/RegisterUser">
+						<p>Not signed up? Click here to make an account!</p>
+					</Link>
+				</form>
+			</div>
+		</div>
+	);
 }
--- server/Routes/userRoutes.js
+++ server/Routes/userRoutes.js
@@ -10,73 +10,70 @@ const router = express.Router();
 
 // needs bcrypt
 router.post("/login", async (req, res) => {
-  const userLogin = await Users.findOne({
-    where: {
-      email: req.body.email,
-    },
-  });
-  console.log(req.body.password, userLogin);
-  if (userLogin) {
-    const passedAuth = await bcrypt.compare(
-      req.body.password,
-      userLogin.password
-    );
-    if (passedAuth) {
-      req.session.id = userLogin.id;
-      res.send(userLogin);
-      return;
-    }
-  }
-  res.status(401).send({ message: "Unauthenticated" });
+	const userLogin = await Users.findOne({
+		where: {
+			email: req.body.email,
+		},
+	});
+	console.log(req.body.password, userLogin);
+	if (userLogin) {
+		const passedAuth = await bcrypt.compare(
+			req.body.password,
+			userLogin.password
+		);
+		if (passedAuth) {
+			req.session.id = userLogin.id;
+			res.send(userLogin);
+			return;
+		}
+	}
+	res.status(401).send({ message: "Unauthenticated" });
 });
 
 router.post("/register", async (req, res) => {
-  if (await Users.findOne({ email: req.body.email })) {
-    res.send({ message: "email in use" });
-  }
+	if (await Users.findOne({ email: req.body.email })) {
+		res.send({ message: "email in use" });
+	}
 
-  console.log(req.body);
-  const {
-    first_name,
-    last_name,
-    email,
-    stripe_acct,
-    password,
-    phone,
-    access,
-    license_plate,
-  } = req.body;
+	console.log(req.body);
+	const {
+		first_name,
+		last_name,
+		email,
+		stripe_acct,
+		password,
+		phone,
+		license_plate,
+	} = req.body;
 
-
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const newUser = await Users.create({
-    first_name: first_name,
-    last_name: last_name,
-    email: email,
-    stripe_acct: stripe_acct,
-    password: hashedPassword,
-    phone: phone,
-    access: access,
-    license_plate: license_plate,
-  });
-  res.send(newUser);
-  console.log(user.id, user.first_name);
+	const hashedPassword = await bcrypt.hash(password, 10);
+	const newUser = await Users.create({
+		first_name: first_name,
+		last_name: last_name,
+		email: email,
+		stripe_customer_id: stripe_acct,
+		password: hashedPassword,
+		phone: phone,
+		license_plate: license_plate,
+	});
+	res.send(newUser);
+	console.log(user.id, user.first_name);
 });
 
 // replace with req.session.id
 router.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  const userData = await Users.findByPk(id);
-  if (userData === null) {
-    return res.send("user not found");
-  } else {
-    return res.json(userData);
-  }
+	const { id } = req.params;
+	const userData = await Users.findByPk(id);
+	if (userData === null) {
+		return res.send("user not found");
+	} else {
+		return res.json(userData);
+	}
 });
 router.get("/", async (req, res) => {
-  req.session.id === null;
-  res.send("logged out")
-})
+	req.session.id === null;
+	res.send("logged out");
+});
 
 // /updateuser (PUT) route needs:
 // * conditionals for grabbing form-fields (if changed, update, else (aka "") don't)
