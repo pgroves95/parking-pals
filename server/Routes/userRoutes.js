ecef20e9803f358f96092e69b03d68e123b0bcf9
Jessica Haugen <j.rose.haugen@gmail.com>
2021-06-19 10:08:48 -0700

    added return
    return statement

--- server/Routes/userRoutes.js
+++ server/Routes/userRoutes.js
@@ -31,8 +31,9 @@ router.post("/login", async (req, res) => {
 });
 
 router.post("/register", async (req, res) => {
-	if (await Users.findOne({ email: req.body.email })) {
+	if (await Users.findOne({ where: { email: req.body.email } })) {
 		res.send({ message: "email in use" });
+		return;
 	}
 
 	console.log(req.body);
@@ -56,8 +57,8 @@ router.post("/register", async (req, res) => {
 		phone: phone,
 		license_plate: license_plate,
 	});
+	console.log(newUser.id, newUser.first_name);
 	res.send(newUser);
-	console.log(user.id, user.first_name);
 });
 
 // replace with req.session.id
