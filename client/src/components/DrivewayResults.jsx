cea725d78ed055818bf9758d5fcf004f3c71d4bd
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-06-22 14:18:09 -0500

    trying map over driveway data

--- client/src/components/DrivewayResults.jsx
+++ client/src/components/DrivewayResults.jsx
@@ -65,6 +65,7 @@ export default function DrivewayResults() {
 	};
 
 	const newLocation = () => {
+		// console.log(`[${dbDrivewayList[0].lat_long[0]}, ${dbDrivewayList[0].lat_long[1]}]`)
 			var map = new mapboxgl.Map({
 				container: 'map',
 				style: 'mapbox://styles/mapbox/streets-v11',
@@ -85,18 +86,18 @@ export default function DrivewayResults() {
 					'data': {
 					'type': 'FeatureCollection',
 					'features': [
-					{
-					'type': 'Feature',
-					'properties': {
-					'description':
-					'<strong>139 Sunset Ave NW, Atlanta, GA 30314</strong><p><a href="http://www.mtpleasantdc.com/makeitmtpleasant" target="_blank" title="Opens in a new window">View More</a></p>',
-					'icon': 'veterinary-15'
-					},
-					'geometry': {
-					'type': 'Point',
-					'coordinates': [-84.409680, 33.758490]
-					}
-					},
+					// {
+					// 'type': 'Feature',
+					// 'properties': {
+					// 'description':
+					// `<strong>${dbDrivewayList[0].address}</strong><p><a href="http://www.mtpleasantdc.com/makeitmtpleasant" target="_blank" title="Opens in a new window">View More</a></p>`,
+					// 'icon': 'veterinary-15'
+					// },
+					// 'geometry': {
+					// 'type': 'Point',
+					// 'coordinates': `[${dbDrivewayList[0].lat_long[0]}, ${dbDrivewayList[0].lat_long[1]}]`
+					// }
+					// },
 					{
 					'type': 'Feature',
 					'properties': {
@@ -192,6 +193,82 @@ export default function DrivewayResults() {
 					});
 					});	
 	};
+
+// 	const newLocation = () => {
+// 		if (searchCoordinates[1]) {
+// 			var map = new mapboxgl.Map({
+// 			container: 'map',
+// 			style: 'mapbox://styles/mapbox/streets-v11',
+// 			center: [searchCoordinates[1], searchCoordinates[0]],
+// 			zoom: 14.5
+// 			});
+// 			let marker1 = new mapboxgl.Marker()
+// 			.setLngLat([searchCoordinates[1], searchCoordinates[0]])
+// 			.addTo(map);
+// 			map.on('load', function () {
+// 				map.addSource('places', {
+// 				// This GeoJSON contains features that include an "icon"
+// 				// property. The value of the "icon" property corresponds
+// 				// to an image in the Mapbox Streets style's sprite.
+// 				'type': 'geojson',
+// 				'data': {
+// 				'type': 'FeatureCollection',
+// 				'features': `${dbDrivewayList.map((driveway) => ({
+// 					'type': 'Feature',
+// 					'properties': {
+// 					'description':
+// 					`<strong>${driveway.address}</strong><p><a href="http://www.mtpleasantdc.com/makeitmtpleasant" target="_blank" title="Opens in a new window">View More</a></p>`,
+// 					'icon': 'veterinary-15'
+// 					},
+// 					'geometry': {
+// 					'type': 'Point',
+// 					'coordinates': `${driveway.lat_long}`
+// 					}
+// 					}))}`
+// 				}
+// 				});
+// 				// Add a layer showing the places.
+// 				map.addLayer({
+// 				'id': 'places',
+// 				'type': 'symbol',
+// 				'source': 'places',
+// 				'layout': {
+// 				'icon-image': '{icon}',
+// 				'icon-allow-overlap': true
+// 				}
+// 				});
+				 
+// 				// When a click event occurs on a feature in the places layer, open a popup at the
+// 				// location of the feature, with description HTML from its properties.
+// 				map.on('click', 'places', function (e) {
+// 				var coordinates = e.features[0].geometry.coordinates.slice();
+// 				var description = e.features[0].properties.description;
+				 
+// 				// Ensure that if the map is zoomed out such that multiple
+// 				// copies of the feature are visible, the popup appears
+// 				// over the copy being pointed to.
+// 				while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+// 				coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+// 				}
+				 
+// 				new mapboxgl.Popup()
+// 				.setLngLat(coordinates)
+// 				.setHTML(description)
+// 				.addTo(map);
+// 				});
+				 
+// 				// Change the cursor to a pointer when the mouse is over the places layer.
+// 				map.on('mouseenter', 'places', function () {
+// 				map.getCanvas().style.cursor = 'pointer';
+// 				});
+				 
+// 				// Change it back to a pointer when it leaves.
+// 				map.on('mouseleave', 'places', function () {
+// 				map.getCanvas().style.cursor = '';
+// 				});
+// 				})}
+// 		;	
+// };
 
 	useEffect(() => {
 		newLocation();
