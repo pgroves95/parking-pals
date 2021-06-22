f0d54afb71b2838a2cbfbc3910461f6d834850e2
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-06-22 10:38:19 -0500

    add clickable icons to map

--- client/src/components/DrivewayResults.jsx
+++ client/src/components/DrivewayResults.jsx
@@ -67,27 +67,127 @@ export default function DrivewayResults() {
 	};
 
 	const newLocation = () => {
-		if (searchCoordinates[1]) {
-			map.current = new mapboxgl.Map({
-				container: mapContainer.current,
-				style: "mapbox://styles/mapbox/streets-v11",
-				center: [searchCoordinates[1], searchCoordinates[0]],
-				zoom: zoom,
-			});
-			let marker1 = new mapboxgl.Marker()
-				.setLngLat([searchCoordinates[1], searchCoordinates[0]])
-				.addTo(map.current);
-		} else {
-			map.current = new mapboxgl.Map({
-				container: mapContainer.current,
-				style: "mapbox://styles/mapbox/streets-v11",
-				center: [lng, lat],
-				zoom: zoom,
-			});
-			let marker2 = new mapboxgl.Marker()
-				.setLngLat([-84.4008875, 33.755288])
-				.addTo(map.current);
-		}
+			var map = new mapboxgl.Map({
+				container: 'map',
+				style: 'mapbox://styles/mapbox/streets-v11',
+				center: [-84.409680, 33.758490],
+				zoom: 14.5
+				});
+				map.on('load', function () {
+					map.addSource('places', {
+					// This GeoJSON contains features that include an "icon"
+					// property. The value of the "icon" property corresponds
+					// to an image in the Mapbox Streets style's sprite.
+					'type': 'geojson',
+					'data': {
+					'type': 'FeatureCollection',
+					'features': [
+					{
+					'type': 'Feature',
+					'properties': {
+					'description':
+					'<strong>139 Sunset Ave NW, Atlanta, GA 30314</strong><p><a href="http://www.mtpleasantdc.com/makeitmtpleasant" target="_blank" title="Opens in a new window">View More</a></p>',
+					'icon': 'veterinary-15'
+					},
+					'geometry': {
+					'type': 'Point',
+					'coordinates': [-84.409680, 33.758490]
+					}
+					},
+					{
+					'type': 'Feature',
+					'properties': {
+					'description':
+					'<strong>603 Delbridge St NW, Atlanta, GA 30314</strong><p><a href="http://madmens5finale.eventbrite.com/" target="_blank" title="Opens in a new window">View More</a></p>',
+					'icon': 'veterinary-15'
+					},
+					'geometry': {
+					'type': 'Point',
+					'coordinates': [-84.407160, 33.757660]
+					}
+					},
+					{
+					'type': 'Feature',
+					'properties': {
+					'description':
+					'<strong>559 Magnolia St NW, Atlanta, GA 30314</strong><p><a href="http://madmens5finale.eventbrite.com/" target="_blank" title="Opens in a new window">View More</a></p>',
+					'icon': 'veterinary-15'
+					},
+					'geometry': {
+					'type': 'Point',
+					'coordinates': [-84.405807, 33.758228]
+					}
+					},
+					{
+					'type': 'Feature',
+					'properties': {
+					'description':
+					'<strong>671 Delbridge St NW, Atlanta, GA 30314</strong><p><a href="http://madmens5finale.eventbrite.com/" target="_blank" title="Opens in a new window">View More</a></p>',
+					'icon': 'veterinary-15'
+					},
+					'geometry': {
+					'type': 'Point',
+					'coordinates': [-84.40829, 33.7577]
+					}
+					},
+					{
+					'type': 'Feature',
+					'properties': {
+					'description':
+					'<strong>601 University Pl NW, Atlanta, GA 30314</strong><p><a href="http://madmens5finale.eventbrite.com/" target="_blank" title="Opens in a new window">View More</a></p>',
+					'icon': 'veterinary-15'
+					},
+					'geometry': {
+					'type': 'Point',
+					'coordinates': [-84.407378
+						, 33.755967
+					]
+					}
+					},
+					
+					]
+					}
+					});
+					// Add a layer showing the places.
+					map.addLayer({
+					'id': 'places',
+					'type': 'symbol',
+					'source': 'places',
+					'layout': {
+					'icon-image': '{icon}',
+					'icon-allow-overlap': true
+					}
+					});
+					 
+					// When a click event occurs on a feature in the places layer, open a popup at the
+					// location of the feature, with description HTML from its properties.
+					map.on('click', 'places', function (e) {
+					var coordinates = e.features[0].geometry.coordinates.slice();
+					var description = e.features[0].properties.description;
+					 
+					// Ensure that if the map is zoomed out such that multiple
+					// copies of the feature are visible, the popup appears
+					// over the copy being pointed to.
+					while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+					coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+					}
+					 
+					new mapboxgl.Popup()
+					.setLngLat(coordinates)
+					.setHTML(description)
+					.addTo(map);
+					});
+					 
+					// Change the cursor to a pointer when the mouse is over the places layer.
+					map.on('mouseenter', 'places', function () {
+					map.getCanvas().style.cursor = 'pointer';
+					});
+					 
+					// Change it back to a pointer when it leaves.
+					map.on('mouseleave', 'places', function () {
+					map.getCanvas().style.cursor = '';
+					});
+					});	
 	};
 
 	useEffect(() => {
@@ -127,7 +227,7 @@ export default function DrivewayResults() {
 						</Paper>
 					</div>)) : <p>No results!</p>}
 				</div>
-				<div ref={mapContainer} className="map-container" />
+				<div id="map"></div>
 			</div>
 			<Footer />
 		</div>
