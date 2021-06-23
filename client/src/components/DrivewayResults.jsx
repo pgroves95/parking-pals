8a4a2a89db80c26d401a51ca24605c2ce37e7dc2
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-06-23 18:51:14 -0500

    add media queries to search page

--- client/src/components/DrivewayResults.jsx
+++ client/src/components/DrivewayResults.jsx
@@ -187,7 +187,7 @@ export default function DrivewayResults() {
 								</div>
 							))
 						) : (
-							<p>No results!</p>
+							<p>Loading...</p>
 						)}
 					</div>
 					<div id="map"></div>
