ae7229d813d781ad27c38e30d7dac625cc13802b
Patrick Groves <patrickalexgroves@gmail.com>
2021-06-26 14:18:44 -0600

    deploy preliminaries

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const config = require('dotenv').config()
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -7,10 +8,16 @@ const pg = require("pg");
 pg.defaults.ssl = true;
 const cookieSession = require("cookie-session");
 const bcrypt = require("bcrypt");
-
 app.use(cors({ origin: (orig, cb) => cb(null, true), credentials: true }));
 
-//cookie-session middlewear
+if(process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('../client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.use(
   cookieSession({
     name: "session",
--- server/sync-db.js
+++
@@ -1,3 +0,0 @@
-const db = require("../models");
-
-db.sequelize.sync({ force: true }).then(() => process.exit(0));
